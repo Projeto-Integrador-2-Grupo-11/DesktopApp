4b2da704fa1ff17c77840259c2a41fbcd4ca1af7
Guilherme Siqueira <guilhersiqueira@gmail.com>
2020-11-19 18:55:34 -0300

    adding MVC structure and connecting with mongodb collections

--- renderer.js
+++ renderer.js
@@ -4,82 +4,18 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
-var Chart = require('chart.js');
-require('chartjs-plugin-labels');
+const mongoose = require('mongoose');
+var quantityOrangesController = require('./controller/quantityOrangesController')
 
-
-
-function splitRgb(rgb) {
-  var matchColors = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
-  var match = matchColors.exec(rgb);
-  var result = match;
-  return result ? {
-    r: parseInt(result[1]),
-    g: parseInt(result[2]),
-    b: parseInt(result[3])
-  } : null;
+async function startDB() {
+  await mongoose.connect('mongodb://localhost:27017/classificador', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }).then(console.log("banco conectado"))
 }
 
+startDB();
 
-// document.getElementById('title').addEventListener('click', startDB);
-var ctx = document.getElementById('myChart');
-var ctx2 = document.getElementById('myChart2');
-
-let quality = {
-  datasets: [{
-    // cria-se um vetor data, com os valores a ser dispostos no gráfico
-    data: [10, 20, 30],
-    // cria-se uma propriedade para adicionar cores aos respectivos valores do vetor data
-    backgroundColor: ['rgb(57, 148, 239)', 'rgb(244, 242, 110)', 'rgb(242, 83, 81)']
-
-  }],
-  // cria-se legendas para os respectivos valores do vetor data
-  labels: ['Boas', 'Manchadas', 'Ruins']
-};
-
-let size = {
-  datasets: [{
-    // cria-se um vetor data, com os valores a ser dispostos no gráfico
-    data: [10, 20, 30],
-    // cria-se uma propriedade para adicionar cores aos respectivos valores do vetor data
-    backgroundColor: ['rgb(55, 99, 132)', 'rgb(255, 199, 132)', 'rgb(255, 99, 132)']
-  }],
-  // cria-se legendas para os respectivos valores do vetor data
-  labels: ['Grandes', 'Médias', 'Pequenas']
-};
-
-let opcoes = {
-  cutoutPercentage: 0,
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    labels: {
-      render: 'value',
-      fontColor: function (data) {
-        var rgb = splitRgb(data.dataset.backgroundColor[data.index]);
-        var threshold = 140;
-        var luminance = 0.299 * rgb.r + 0.587 * rgb.g + 0.114 * rgb.b;
-        return luminance > threshold ? 'black' : 'white';
-      },
-      precision: 2
-    }
-  },
-  legend: {
-    position: 'bottom',
-    labels: {
-      usePointStyle: true
-    }
-  }
-};
-
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: quality,
-  options: opcoes
-});
-
-var myPieChart2 = new Chart(ctx2, {
-  type: 'pie',
-  data: size,
-  options: opcoes
-});
+quantityOrangesController.getAllOranges();

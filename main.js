6b6770cc9cd29641ef77496e628beccb5da3e836
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2020-11-22 16:23:47 -0300

    Criação da funções de sincrozinização de banco

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-// Modules to control application life and create native browser window
+//----IMPORTS----
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const mongoose = require('mongoose');
 const firebase = require("firebase/app");
-
+var QuantityOranges = require('./model/quantityOrangesModel');
+var Oranges = require('./model/orangesModel');
 require("firebase/auth");
 require("firebase/firestore");
 
+//----VARIABLES----
+const TIME = 1800; //TIME IN SECONDS
 const firebaseConfig = {
   apiKey: "AIzaSyCboa08hL9h2vVIKp11gvNDCgwxCyhztI4",
   authDomain: "seletor-frutas.firebaseapp.com",
@@ -16,6 +19,8 @@ const firebaseConfig = {
   messagingSenderId: "687514495840"
 };
 
+
+//----FUNCTIONS----
 async function startDB() {
   // console.log("au")
   await mongoose.connect('mongodb://localhost:27017/classificador', {
@@ -23,11 +28,13 @@ async function startDB() {
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
-  }).then(console.log("banco conectado"))
+  }).then(console.log("DATA BASE CONNECTED"))
 }
 
-function syncDataBase(){
-  console.log('SYNC');
+function syncDataBase() {
+  console.log('SYNCING DATA');
+  sendQuantityOrangesToFirebase();
+  sendOrangesToFirebase();
 }
 
 function checkInternetConection() {
@@ -42,36 +49,58 @@ function checkInternetConection() {
   });
 };
 
-function sendDataToFirebase(collection) {
-
-  // var array = [
-  //   {
-  //     batch: 1,
-  //     classification: 'good_with_spots',
-  //     date: '8 de outubro de 2020 15:00:00 UTC-3',
-  //     imgs: ["img1", "img2"],
-  //     machine_id: 1
-  //   },
-  //   {
-  //     batch: 1,
-  //     classification: 'good_without_spots',
-  //     date: '8 de outubro de 2020 15:15:00 UTC-3',
-  //     imgs: ["img1", "img2"],
-  //     machine_id: 1
-  //   }
-  // ]
+function sendOrangesToFirebase() {
 
   var db = firebase.firestore();
-  var batch = db.batch();
 
-  collection.forEach((doc) => {
-    var docRef = db.collection("oranges").doc(); //automatically generate unique id
-    batch.set(docRef, doc);
-  });
+  Oranges.find({}).then((oranges) => {
 
-  batch.commit().then((ref) => {
-    console.log("Added batch: ", ref);
+    var obj; 
+    var batch = db.batch();
+
+    oranges.forEach((doc) => {
+
+      obj = {
+        "batch": doc.batch,
+        "classification": doc.classification,
+        "imgs": doc.imgs,
+        "machine_id": doc.machine_id
+      }
+
+      batch.set(db.collection("oranges").doc(doc._id.toString()), Object.assign({}, obj));
+    });
+
+    batch.commit().then((ref) => {
+      console.log("Added batch: ", ref);
+    });
   });
+}
+
+function sendQuantityOrangesToFirebase() {
+  var db = firebase.firestore();
+  var obj;
+
+  QuantityOranges.find({}).sort({ _id: -1 }).limit(1).then((lastInserted) => {
+
+    var obj = {
+      // "_id": lastInserted[0]._id,
+      "batch": lastInserted[0].batch,
+      "date": lastInserted[0].date,
+      "large_oranges": lastInserted[0].large_oranges,
+      "machine_id": lastInserted[0].machine_id,
+      "medium_oranges": lastInserted[0].medium_oranges,
+      "small_oranges": lastInserted[0].small_oranges
+    }
+
+    var identificator = lastInserted[0]._id;
+
+    if (lastInserted[0]._id) {
+      db.collection("quantity_oranges").doc(identificator.toString()).set(Object.assign({}, obj));
+      // console.log(lastInserted[0]._id);
+    }
+
+  })
+
 }
 
 function createWindow() {
@@ -96,12 +125,12 @@ app.whenReady().then(() => {
 
   startDB();
   firebase.initializeApp(firebaseConfig);
+
   createWindow();
-  // sendDataToFirebase();
+
   setInterval(async () => {
     checkInternetConection();
-  }, 1800);
-  checkInternetConection();
+  }, TIME);
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
